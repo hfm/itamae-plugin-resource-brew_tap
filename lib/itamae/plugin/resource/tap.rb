c4b3b05527a061b48bb9c4b3574232d74d3a7fa9
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-09-10 05:15:24 +0900

    :tada: bundle gem itamae-plugin-resource-tap

---
+++ lib/itamae/plugin/resource/tap.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/tap/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module Tap
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/resource/tap/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Resource
+      module Tap
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
